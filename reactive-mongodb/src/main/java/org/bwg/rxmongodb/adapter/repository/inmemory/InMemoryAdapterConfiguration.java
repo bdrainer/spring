e74eddff35edc9f7cc198137d3974b9d71b3b969
Brett Drainer <bdrainer@gmail.com>
2023-11-24 07:09:56 -0800

    Added an in-memory repository implementation

--- reactive-mongodb/src/main/java/org/bwg/rxmongodb/adapter/repository/inmemory/InMemoryAdapterConfiguration.java
+++ reactive-mongodb/src/main/java/org/bwg/rxmongodb/adapter/repository/inmemory/InMemoryAdapterConfiguration.java
@@ -1,7 +1,9 @@
 package org.bwg.rxmongodb.adapter.repository.inmemory;
 
 import lombok.extern.slf4j.Slf4j;
+import org.bwg.rxmongodb.port.ProfileRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
@@ -11,5 +13,10 @@ public class InMemoryAdapterConfiguration {
 
     public InMemoryAdapterConfiguration() {
         log.info("[app.repository.provider] = inmemory");
+    }
+
+    @Bean
+    public ProfileRepository profileRepository() {
+        return new InMemoryProfileRepository();
     }
 }
--- reactive-mongodb/src/main/java/org/bwg/rxmongodb/adapter/repository/inmemory/InMemoryProfileRepository.java
+++ reactive-mongodb/src/main/java/org/bwg/rxmongodb/adapter/repository/inmemory/InMemoryProfileRepository.java
@@ -1,5 +1,6 @@
 package org.bwg.rxmongodb.adapter.repository.inmemory;
 
+import org.bwg.rxmongodb.model.exception.RecordNotFoundException;
 import org.bwg.rxmongodb.model.profile.Profile;
 import org.bwg.rxmongodb.model.profile.ProfileType;
 import org.bwg.rxmongodb.port.ProfileRepository;
@@ -9,20 +10,25 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Stores profiles in memory.  Intended for local development only.
+ */
 public class InMemoryProfileRepository implements ProfileRepository {
 
-    private static final Map<String, Profile> DATABASE = Collections.synchronizedMap(new HashMap<>());
+    private final Map<String, Profile> DATABASE = Collections.synchronizedMap(new HashMap<>());
 
     @Override
     public Mono<Profile> save(String username, Profile profile) {
-        DATABASE.replace(toKey(username, profile), profile);
+        DATABASE.put(toKey(username, profile), profile);
         return Mono.just(profile);
     }
 
     @Override
     public Mono<Profile> findByUniqueKey(String username, ProfileType type, String userkey) {
         String key = toKey(username, type, userkey);
-        return DATABASE.containsKey(key) ? Mono.just(DATABASE.get(key)) : Mono.empty();
+        return DATABASE.containsKey(key)
+                ? Mono.just(DATABASE.get(key))
+                : Mono.error(new RecordNotFoundException("Profile not found"));
     }
 
     private String toKey(String username, Profile profile) {
