f6c1592ef31dceb0065101f11ece7a094de47ff7
Brett Drainer <bdrainer@gmail.com>
2024-01-10 13:32:22 -0800

    Added "produces" to the ApplicationProfileController

--- reactive-mongodb/src/main/java/org/bwg/rxmongodb/application/ApplicationProfileController.java
+++ reactive-mongodb/src/main/java/org/bwg/rxmongodb/application/ApplicationProfileController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bwg.rxmongodb.model.profile.Profile;
 import org.bwg.rxmongodb.model.profile.ProfileType;
 import org.bwg.rxmongodb.port.ProfileRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,19 +29,19 @@ public class ApplicationProfileController {
         return repository.save(username, payload);
     }
 
-    @GetMapping("/{username}/{type}/{userkey}")
+    @GetMapping(value = "/{username}/{type}/{userkey}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Profile> getProfileByUniqueKey(@PathVariable String username, @PathVariable String type, @PathVariable String userkey) {
         log.debug("Get profile {}, {}, {}", username, type, userkey);
         return repository.findByUniqueKey(username, ProfileType.valueOf(type.toUpperCase()), userkey);
     }
 
-    @GetMapping("/{username}")
+    @GetMapping(value = "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Profile> getProfileByUsername(@PathVariable String username) {
         log.debug("Get profiles for user {}", username);
         return repository.findByUsername(username);
     }
 
-    @GetMapping("/{username}/{type}")
+    @GetMapping(value = "/{username}/{type}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Profile> getProfileByUsernameAndType(@PathVariable String username, @PathVariable String type) {
         log.debug("Get profiles {} for user {}", type, username);
         return repository.findByUsernameAndType(username, ProfileType.valueOf(type.toUpperCase()));
